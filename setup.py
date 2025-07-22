495e778780dd7f034c78bfa0b4d3af79fd8a3134
lsm0729 <lsm0729@bos-semi.com>
2025-07-22 09:09:09 +0900

    ..

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name="my_quant_lib",
+    version="0.1.0",
+    description="Python bindings for MyQuantLib",
+    author="SunginminLee",
+    packages=find_packages(where="python"),
+    package_dir={"": "python"},
+    package_data={"my_quant_lib": ["*.so"]},
+    include_package_data=True,
+    install_requires=[
+    ],
+    zip_safe=False,
+)
